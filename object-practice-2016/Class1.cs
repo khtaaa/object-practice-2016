446983cf504894356c59b9ba0514e8f45ca1197f
khtaaa <kahata1128@gmail.com>
2017-02-17 11:12:07 +0900

    クラスを作成

---
+++ object-practice-2016/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_practice_2016
+{
+    class Class1
+    {
+    }
+}
